2be28f7cfca553be11bc5814c938a784cd57ab05
SayYass <ilyasdewanto@gmail.com>
2022-01-18 17:15:39 +0700

    listen

--- app.js
+++ app.js
@@ -16,5 +16,5 @@ app.use(( req, res, next ) => {
     });
 });
 
-app.listen(3000, () => console.log('Server http://localhost:3000'))
+app.listen('sayyas-express.herokuapp.com', () => console.log('Server http://localhost:3000'))
 
