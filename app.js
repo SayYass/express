e39f36f7065c62883e4b4094e0ecac2318b630ec
SayYass <ilyasdewanto@gmail.com>
2022-01-18 17:36:31 +0700

    ...

--- app.js
+++ app.js
@@ -2,11 +2,10 @@ const express = require('express');
 const router = require('./routes');
 const log = require('./middleware/logger')
 const path = require('path')
-const https = require('https')
 
 const app = express();
 app.use(express.urlencoded({extended:true}))
-app.use('/public' , express.static(path.join(__dirname, 'uploads')));
+app.use.apply('/public' , express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
 app.use(router);
 app.use(( req, res, next ) => {
@@ -17,7 +16,5 @@ app.use(( req, res, next ) => {
     });
 });
 
-app.listen = function () {
-    var server = https.createServer(this)
-    return server.listen.apply(server, arguments)
-  }
+app.listen(3000, () => console.log('Server http://localhost:3000'))
+
