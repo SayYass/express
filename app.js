bc54036e0c90698a0fece3d27c87c324c09bbe7f
SayYass <ilyasdewanto@gmail.com>
2022-01-18 17:48:30 +0700

    initiate

--- app.js
+++ app.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = require('./routes');
 const log = require('./middleware/logger')
 const path = require('path')
+const Port = 3000;
 
 const app = express();
 app.use(express.urlencoded({extended:true}))
-app.use.apply('/public' , express.static(path.join(__dirname, 'uploads')));
+app.use('/public' , express.static(path.join(__dirname, 'uploads')));
 app.use(express.json());
 app.use(router);
 app.use(( req, res, next ) => {
@@ -16,5 +17,5 @@ app.use(( req, res, next ) => {
     });
 });
 
-app.listen(3000, () => console.log('Server http://localhost:3000'))
+app.listen(process.env.PORT || Port, () => console.log(`Server http://localhost:3000`))
 
