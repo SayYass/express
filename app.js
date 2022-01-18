06c20640e971721d6506ab60dcaf0d8db82138d7
SayYass <ilyasdewanto@gmail.com>
2022-01-18 17:06:22 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = require('./routes');
+const log = require('./middleware/logger')
+const path = require('path')
+
+const app = express();
+app.use(express.urlencoded({extended:true}))
+app.use('/public' , express.static(path.join(__dirname, 'uploads')));
+app.use(express.json());
+app.use(router);
+app.use(( req, res, next ) => {
+    res.status(404);
+    res.send({
+        status : 'Failed',
+        message : 'Resource ' +  req.originalUrl + ' NotFound'
+    });
+});
+
+app.listen(3000, () => console.log('Server http://localhost:3000'))
+
